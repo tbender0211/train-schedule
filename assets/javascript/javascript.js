82d22efd1d20fb2bf1791b44e161ca71d31f7f4f
Cassindi <tara.devin.b@gmail.com>
2018-06-25 18:05:12 -0500

    mostly html

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,10 @@
+$("#submit").on("click", function(){
+    
+    var trainName = $("#train-name").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrain = $("#first-train").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+    console.log(firstTrain);
+
+})
