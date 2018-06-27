1f57fa5dabd699f67c6a6e79dfa09e02f249f6f6
Cassindi <tara.devin.b@gmail.com>
2018-06-26 22:25:57 -0500

    dumb firebase

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,50 @@
-$("#submit").on("click", function(){
+var config = {
+    apiKey: "AIzaSyCJBjsI9xc4RhgFkM1febqED3LSmwTFyAY",
+    authDomain: "train-schedule-5ca0b.firebaseapp.com",
+    databaseURL: "https://train-schedule-5ca0b.firebaseio.com",
+    projectId: "train-schedule-5ca0b",
+    storageBucket: "train-schedule-5ca0b.appspot.com",
+    messagingSenderId: "533130741236"
+  };
+  firebase.initializeApp(config);
+
+var trainName = "";
+var destination = "";
+var firstTrain = "";
+var frequency = "";
+var nextTrain = "";
+var minAway = "";
+var database = firebase.database();
+
+$("#submit").on("click", function(event){
+
+    event.preventDefault();
     
     var trainName = $("#train-name").val().trim();
     var destination = $("#destination").val().trim();
     var firstTrain = $("#first-train").val().trim();
     var frequency = $("#frequency").val().trim();
+    var nextTrain = "";
+    var minAway = "";
 
+    console.log(trainName);
+    console.log(destination);
     console.log(firstTrain);
+    console.log(frequency);
+
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        nextTrain: nextTrain,
+        minAway: minAway,
+    })
+
+})
+
+database.ref().on("child_added", function(childSnapshot){
+
+    $("#train-schedule").append("<tr><td>" + childSnapshot.val().trainName + "</td><td>" + childSnapshot.val().destination + "</td><td>" + childSnapshot.val().firstTrain + "</td><td>" + childSnapshot.val().frequency + "</td><td>" + nextTrain + "</td><td>" + minAway + "</td></tr>");
 
 })
